fa46db5e880af4bcf47b66a3e3d00aad1dfc48db
maxrosario99 <maxumum1000@gmail.com>
2024-04-26 14:20:54 -0400

    fixed section issue

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,24 +4,36 @@ import {usePathname} from "next/navigation"
 import { sidebarLinks } from '@/app/(root)/constants';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import { link } from 'fs';
 const Sidebar = () => {
     const pathname = usePathname();
   return (
     <section className="sticky left-0 top -0 flex h-screen w-fit flex-col
     justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264]">
         <div className="flex flex- flex-col gap-6">
-            {sidebarLinks.map((item) =>{
-                const isActive = pathname === item.route || pathname.startsWith(item.route);
+            {sidebarLinks.map((link) =>{
+                const isActive = pathname === link.route || pathname.startsWith(link.route);
                 return(
                     <Link
-                        href={item.route}
-                        key={item.label}
-                        className={cn("flex gap-4 items-center p-4 rounded-lg justify-start", {
+                        href={link.route}
+                        key={link.label}
+                        className={cn("flex gap-4 links-center p-4 rounded-lg justify-start", {
                             "bg-blue-1": isActive,
                         })}
                     >
-                        {item.label}
-                    </Link>
+                        <Image
+                        src ={link.imgUrl}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                        
+                        />
+                        <p className='text-lg font-semibold max-lg:hidden'>
+
+                        </p>
+    
+                        </Link>
                 )
 })
                 
