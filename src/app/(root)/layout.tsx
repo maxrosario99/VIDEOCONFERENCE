cafd091e2a52a746192ebb9acd36844adf8410ea
maxrosario99 <maxumum1000@gmail.com>
2024-04-27 15:47:05 -0400

    added new routing

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,43 @@
 import React, {ReactNode} from 'react'
+import { ClerkProvider } from "@clerk/nextjs";
+const inter = Inter({ subsets: ["latin"] });
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/toaster";
 
-const RootLayout = ({children}: {children: 
-    ReactNode}) => {
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
-<main>
-    Navbar
-    {children}
-    Footer
-    </main>
-  )
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/yoom-logo.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          <Toaster />
+          {children}
+        </body>
+      </ClerkProvider>
+    </html>
+  );
 }
-
-export default RootLayout
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,16 +1,23 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetDescription,
     SheetHeader,
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
+import { sidebarLinks } from '@/app/(root)/constants'
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
     <section className="w-full max-w-[264]">
         <Sheet>
@@ -34,7 +41,42 @@ const MobileNav = () => {
         
         />
         </Link>
+    <div className='flex h-[calc(100vh-72px)]
+    flex-col justify-between overflow-y-auto'>
+      <SheetClose asChild>
+        <section className='flex h-full flex-col
+        gap-6 pt-16 text-white'>
+
+          {sidebarLinks.map((link) =>{
+                const isActive = pathname === link.route ;
+                return(
+                  <SheetClose asChild key={link.route}>
+                    <Link
+                        href={link.route}
+                        key={link.label}
+                        className={cn("flex gap-4 links-center p-4 rounded-lg w-full max-w-60", {
+                            "bg-blue-1": isActive,
+                        })}
+                    >
+                        <Image
+                        src ={link.imgUrl}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                        
+                        />
+                        <p className='text-lg font-semibold max-lg:hidden'>
+
+                        </p>
     
+                        </Link>
+                        </SheetClose>
+                )
+})}
+        </section>
+
+      </SheetClose>
+    </div>
   </SheetContent>
 </Sheet>
     </section>
