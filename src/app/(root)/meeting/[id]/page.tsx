8e9d82a84beaf22a9e21e093877b2ec81dfe0053
maxrosario99 <maxumum1000@gmail.com>
2024-04-28 11:08:33 -0400

    fix client

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react'
 
 const Meeting = ({params}: {params: {id: string }}) => {
+  const { user, isLoaded } useUser();
   return (
     <div>Meeting Room: #{params.id} </div>
   )
